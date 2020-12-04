4aba63f8ddd63b04d1ac732c0c4e8ce640cd47a6
Suhaas D <suhasdgupta55555@gmail.com>
2020-12-04 19:03:19 +0530

    Create TheKthFactorOfN.java

---
+++ TheKthFactorOfN.java
@@ -0,0 +1,14 @@
+class TheKthFactorOfN {
+    public int kthFactor(int n, int k) {
+        int cnt = 0;
+        List<Integer> l = new ArrayList<>();
+        for(int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i != n) l.add(n / i);
+                if (++cnt == k) return i;
+            }
+        }
+        if (l.size() + cnt < k) return -1;
+        return l.get(l.size() - (k - cnt));
+    }
+}
